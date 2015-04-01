ab394626ac0f4bc5e9f77aea5e76cbcf1de4e668
Spoetnic <pcaapje@gmail.com>
2015-04-01 12:46:33 +0200

    update song model and changed app name

--- IUXE2015/app/src/main/java/com/undefined/iuxe2015/model/Song.java
+++ IUXE2015/app/src/main/java/com/undefined/iuxe2015/model/Song.java
@@ -3,8 +3,10 @@ package com.undefined.iuxe2015.model;
 /**
  * Created by Jan-Willem on 1-4-2015.
  */
-public class Music {
+public class Song {
     private String genre;
     private String title;
     private String artist;
+    private String uri;
+    private int duration;
 }
