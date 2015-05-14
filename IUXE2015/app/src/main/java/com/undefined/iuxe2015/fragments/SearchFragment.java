14727b022fdaf4e95f75205d3fb2a91d22f0b59d
Spoetnic <pcaapje@gmail.com>
2015-05-15 01:16:39 +0200

    Fix Derp

--- IUXE2015/app/src/main/java/com/undefined/iuxe2015/fragments/SearchFragment.java
+++ IUXE2015/app/src/main/java/com/undefined/iuxe2015/fragments/SearchFragment.java
@@ -100,11 +100,11 @@ public class SearchFragment extends MumoFragment {
 
                     @Override
                     public void onConnectionFailed(Exception e) {
-                        //TODO give ui feedback about what went wrong
                         Log.d("SearchFragment", "onConnectionFailed");
                         searchEmpty.setVisibility(View.VISIBLE);
                         searchHeader.setVisibility(View.INVISIBLE);
                         searchEmpty.setText(R.string.search_connection_failed);
+                        adapter.refresh(null);
                     }
                 });
             }
