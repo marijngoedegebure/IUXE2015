b5f4e51bfbf3bda4fc855aa9e7540d13d62024a1
Ralf Nieuwenhuizen <ralfnieuwenhuizen@gmail.com>
2015-06-02 16:19:57 +0200

    Create test data, not sure if works

--- IUXE2015/app/src/main/java/com/undefined/iuxe2015/model/persistent/MumoDbHelper.java
+++ IUXE2015/app/src/main/java/com/undefined/iuxe2015/model/persistent/MumoDbHelper.java
@@ -5,6 +5,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.undefined.iuxe2015.model.Event;
+import com.undefined.iuxe2015.model.Rating;
+import com.undefined.iuxe2015.model.Song;
+import com.undefined.iuxe2015.model.Stakeholder;
+import com.undefined.iuxe2015.model.persistent.helpers.Create;
+import com.undefined.iuxe2015.tools.ConnectionTool;
+
 /**
  * Created by Jan-Willem on 8-4-2015.
  */
@@ -12,6 +19,7 @@ public class MumoDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "mumo.db";
     private static final int DATABASE_VERSION = 10;
+    private Context context = null;
 
     public static final String TABLE_RATINGS = "ratings";
     public static final String RATINGS_COLUMN_ID = "_id";
@@ -169,6 +177,7 @@ public class MumoDbHelper extends SQLiteOpenHelper {
 
     public MumoDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
@@ -200,6 +209,86 @@ public class MumoDbHelper extends SQLiteOpenHelper {
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
         clear(db);
+        insertTestData(db);
+    }
+
+    private void insertTestData(SQLiteDatabase db) {
+        onCreate(db);
+
+        // Stakeholder 1
+        Stakeholder s1 = Create.stakeholder(db, "Stakeholder 1", 86);
+        Event s1_e1 = Create.event(db, s1.get_id(), "Bevrijding van Nederland", 1945);
+        addRatingForSong(db, s1.get_id(), s1_e1.get_id(), "2ueOVitGw6YDUtz8pkzLi6", 7);
+        Event s1_e2 = Create.event(db, s1.get_id(), "New Orleans", 1997);
+        addRatingForSong(db, s1.get_id(), s1_e2.get_id(), "6dZKWYSx5YBIme4SfpIHJ0", 6);
+        Event s1_e3 = Create.event(db, s1.get_id(), "Radio", 1980);
+        addRatingForSong(db, s1.get_id(), s1_e3.get_id(), "6HjWcJxjyGK6enWwEjJ5YO", 5);
+
+        // Stakeholder 2
+        Stakeholder s2 = Create.stakeholder(db, "Stakeholder 2", 86);
+        Event s2_e1 = Create.event(db, s2.get_id(), "Delft", 1940);
+        addRatingForSong(db, s2.get_id(), s2_e1.get_id(), "2ueOVitGw6YDUtz8pkzLi6", 5);
+        Event s2_e2 = Create.event(db, s2.get_id(), "Dansles", 0);
+        addRatingForSong(db, s2.get_id(), s2_e2.get_id(), "3XiFWZoHQtGUYIdtShPwPD", 6);
+        Event s2_e3 = Create.event(db, s2.get_id(), "Jaren 50", 1950);
+        addRatingForSong(db, s2.get_id(), s2_e3.get_id(), "2VfvfFSVD1Ki0PH0Xd0jcv", 5);
+
+        // Stakeholder 3
+        Stakeholder s3 = Create.stakeholder(db, "Stakeholder 3", 73);
+        Event s3_e1 = Create.event(db, s3.get_id(), "Kantoor", 0);
+        addRatingForSong(db, s3.get_id(), s3_e1.get_id(), "0ITl2fIq5CXY6xJxSX7xcj", 4);
+        addRatingForSong(db, s3.get_id(), s3_e1.get_id(), "2D5Nm1T3X3g3fwHmofslcI", 4);
+        addRatingForSong(db, s3.get_id(), s3_e1.get_id(), "4GsPt5QlblrVMSWzt3ZGmQ", 4);
+        Event s3_e2 = Create.event(db, s3.get_id(), "Dansles", 0);
+        addRatingForSong(db, s3.get_id(), s3_e2.get_id(), "0jxPcl13TZE3AsMF1DFg9l", 6);
+
+        // Stakeholder 4
+        Stakeholder s4 = Create.stakeholder(db, "Stakeholder 4", 82);
+        Event s4_e1 = Create.event(db, s4.get_id(), "Nieuw-Zeeland", 1992);
+        addRatingForSong(db, s4.get_id(), s4_e1.get_id(), "7utRJ4BeYx85khzP3lKoBX", 5);
+        Event s4_e2 = Create.event(db, s4.get_id(), "Dansles", 1955);
+        addRatingForSong(db, s4.get_id(), s4_e2.get_id(), "6NegwLFCxkSyrS8RkC5YtA", 6);
+        Event s4_e3 = Create.event(db, s4.get_id(), "Het koor", 0);
+        addRatingForSong(db, s4.get_id(), s4_e3.get_id(), "0hkAZrICOGp0NOvyqqqNfU", 6);
+        addRatingForSong(db, s4.get_id(), s4_e3.get_id(), "1OcbMwsJswmPn0d1gvnqH0", 6);
+        addRatingForSong(db, s4.get_id(), s4_e3.get_id(), "147L3RE4rK5wWgCTqITKi6", 6);
+        addRatingForSong(db, s4.get_id(), s4_e3.get_id(), "2EPhNDKWUEazabFnHU3oCX", 6);
+        addRatingForSong(db, s4.get_id(), s4_e3.get_id(), "4GsPt5QlblrVMSWzt3ZGmQ", 6);
+        addRatingForSong(db, s4.get_id(), s4_e3.get_id(), "0jxPcl13TZE3AsMF1DFg9l", 6);
+        Event s4_e4 = Create.event(db, s4.get_id(), "Familie", 0);
+        addRatingForSong(db, s4.get_id(), s4_e4.get_id(), "6bPeMkdmAvWXJARpy2X2UP", 6);
+
+        // Stakeholder 5
+        Stakeholder s5 = Create.stakeholder(db, "Stakeholder 2", 86);
+        Event s5_e1 = Create.event(db, s5.get_id(), "Moldau", 0);
+        addRatingForSong(db, s5.get_id(), s5_e1.get_id(), "3SRqUYfMhO2V7d8XJHrfP8", 5);
+        Event s5_e2 = Create.event(db, s5.get_id(), "Opera", 0);
+        addRatingForSong(db, s5.get_id(), s5_e2.get_id(), "6kuMsDgBeF0j2qra1ijJkL", 6);
+        Event s5_e3 = Create.event(db, s5.get_id(), "Bijbel liederen", 0);
+        //addRatingForSong(db, s5.get_id(), s5_e3.get_id(), "", 5);
+        Event s5_e4 = Create.event(db, s5.get_id(), "Kinderen", 0);
+        addRatingForSong(db, s5.get_id(), s5_e4.get_id(), "25uqTshwBaiZD1yVNrDcVx", 5);
+    }
+
+    private void addRatingForSong(final SQLiteDatabase db, final int stakeholderId, final int eventId, String songId, final int rating) {
+        ConnectionTool.getSong(context, songId, new ConnectionTool.ConnectionSongListener() {
+            @Override
+            public void onConnectionSuccess(Song result) {
+                //TODO stop loading UI, if visible
+                if (result != null) {
+                    Log.d("insertTestData", "onConnectionSuccess:" + result);
+                    Song s = Create.song(db, stakeholderId, result);
+                    Rating r = Create.rating(db, stakeholderId, s.get_id(), eventId, rating, "");
+                } else {
+                    Log.d("insertTestData", "onConnectionSuccess: but no results");
+                }
+            }
+
+            @Override
+            public void onConnectionFailed(Exception e) {
+                Log.d("insertTestData", "onConnectionFailed");
+            }
+        });
     }
 }
 
